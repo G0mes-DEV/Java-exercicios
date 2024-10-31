b5edbdfd7bd5d598914d67d516ed76b99782a170
Kauan Vasconcelos Gomes <kauanvasconcelos169@gmail.com>
2024-10-31 16:54:42 -0300

    Update ex04.java

--- ex04/src/ex04.java
+++ ex04/src/ex04.java
@@ -1,20 +1,46 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package ex04;
+VAR
+HorasNo,HorasEx,SalarioBruto,SalarioLiquido
+INICIO 
 
-/**
- *
- * @author ACER
- */
-public class Ex04 {
+LER HorasNo
+LER HorasEx 
 
-    /**
-     * @param args the command line arguments
-     */
+SalarioBruto = (HorasNo * 10)+(HorasEx * 15)
+SalarioLiquido = (SalarioBruto-(SalarioBruto * 0,1)) 
+
+ESCREVA "O Salário Bruto do funcionário é "+SalarioBruto
+ESCREVA "O Salário Líquido do funcionário é "+SalarioLiquido 
+
+FIM 
+
+Teste de Mesa 
+
+| HorasNo | | HorasEx | | SalarioBruto | | SalarioLiquido | | ESCREVA | | ESCREVA |
+
+ */
+
+import javax.swing.JOptionPane;
+
+public class ex04 {
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        double horasNo,horasEx,salarioBruto,salarioLiquido;
+
+        String s_horasno,s_horasEx;
+
+        s_horasno = JOptionPane.showInputDialog("Quantas horas você trabalhou? ");
+        horasNo = Double.parseDouble(s_horasno);
+
+        s_horasEx = JOptionPane.showInputDialog("Quantas horas extras você trabalhou? ");
+        horasEx = Double.parseDouble(s_horasEx);
+
+        salarioBruto =  (horasNo * 10)+(horasEx * 15);
+        salarioLiquido = salarioBruto - (salarioBruto * 0.1);
+
+        JOptionPane.showMessageDialog(null,"O Salário Bruto do funcionário é: " + salarioBruto);
+        JOptionPane.showMessageDialog(null,"O Salário Líquido do funcionário é: " + salarioLiquido);
+
+        
     }
-    
 }
